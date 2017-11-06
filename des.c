4337ed3d6bda834cb50a2dfcacb6adad5c925742
Alex Erwin <gerwin@email.arizona.edu>
2017-11-06 00:49:05 -0700

    everything

--- des.c
+++ des.c
@@ -48,6 +48,13 @@ static const int initialPermutation[64] = {
    57, 49, 41, 33, 25, 17,  9,  1, 59, 51, 43, 35, 27, 19, 11,  3,
    61, 53, 45, 37, 29, 21, 13,  5, 63, 55, 47, 39, 31, 23, 15,  7
 };
+// expansion permutation table
+static const int expansionPermutation[48] = {
+   32,  1,  2,  3,  4,  5,  4,  5,  6,  7,  8,  9,
+    8,  9, 10, 11, 12, 13, 12, 13, 14, 15, 16, 17,
+   16, 17, 18, 19, 20, 21, 20, 21, 22, 23, 24, 25,
+   24, 25, 26, 27, 28, 29, 28, 29, 30, 31, 32,  1
+};
 // final permutation table
 static const int finalPermutation[64] = {
    40,  8, 48, 16, 56, 24, 64, 32, 39,  7, 47, 15, 55, 23, 63, 31,
@@ -55,7 +62,53 @@ static const int finalPermutation[64] = {
    36,  4, 44, 12, 52, 20, 60, 28, 35,  3, 43, 11, 51, 19, 59, 27,
    34,  2, 42, 10, 50, 18, 58, 26, 33,  1, 41,  9, 49, 17, 57, 25
 };
+// sbox table
+static const int sbox[8][4][16] = {
+   {{14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7},
+   { 0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8},
+   { 4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0},
+   {15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13}},
 
+   {{15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10},
+   { 3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5},
+   { 0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15},
+   {13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9}},
+
+   {{10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8},
+   {13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1},
+   {13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7},
+   { 1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12}},
+
+   {{ 7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15},
+   {13,  8, 11,  5,  6, 15,  0,  3,  4,  7,  2, 12,  1, 10, 14,  9},
+   {10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4},
+   { 3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14}},
+
+   {{ 2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9},
+   {14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6},
+   { 4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14},
+   {11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3}},
+
+   {{12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11},
+   {10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8},
+   { 9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6},
+   { 4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13}},
+
+   {{ 4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1},
+   {13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6},
+   { 1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2},
+   { 6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12}},
+
+   {{13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7},
+   { 1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2},
+   { 7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8},
+   { 2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11}}
+};
+// straight pbox
+static const int pbox[32] = {
+   16,  7, 20, 21, 29, 12, 28, 17,  1, 15, 23, 26,  5, 18, 31, 10,
+    2,  8, 24, 14, 32, 27,  3,  9, 19, 13, 30,  6, 22, 11,  4, 25
+};
 // get bit at pos
 int getBit(unsigned char *src, int pos) {
 	// set mask
@@ -76,6 +129,18 @@ void setBit(unsigned char *dst, int pos, int val) {
 	   dst[pos / 8] = dst[pos / 8] | mask;
 	else
 	   dst[pos / 8] = dst[pos / 8] & (~mask);
+}
+// xor bits
+void xorBit(const unsigned char *plainText, const unsigned char *subKeyText, unsigned char *xorText, int size) {
+	// variables
+	int i;
+	// xor the plaintext and subKeyText to yield the xorText
+	for (i = 0; i < size; i++) {
+	   if (getBit(plainText, i) != getBit(subKeyText, i))
+		  setBit(xorText, i, 1);
+	   else
+		  setBit(xorText, i, 0);
+	}
 }
 // push and set bits in rotations
 void pushBit(unsigned char *data, int size, int count) {
@@ -99,13 +164,6 @@ void pushBit(unsigned char *data, int size, int count) {
 		setBit(data, size - 1, msBit);
 	}
 }
-
-
-
-
-
-
-
 // perform permutation according to the table
 void doPermutation(unsigned char *data, const int *pArray, int count) {
 	// variables
@@ -119,7 +177,7 @@ void doPermutation(unsigned char *data, const int *pArray, int count) {
 	// set return
 	memcpy(data, temp, 8);
 }
-// expand & permute
+/*// expand & permute
 uint8_t * expandPBox (unsigned int v[1]) {
 	// memory storage for returning the expanded p box
 	unsigned int pbox[2];
@@ -140,7 +198,7 @@ printf("%d\n", pbox[0]);
 		B_TOGGLE(pbox[1], 5);
 printf("%d\n", pbox[1]);
 	return (uint8_t *)pbox;
-}
+}*/
 
 
 void hexdump_to_string(const void *data, int size, char *str) {
@@ -160,6 +218,11 @@ void des_enc(uint32_t v[2], uint32_t const key[2]) {
 void des_dec(uint32_t v[2], uint32_t const key[2]) {
 	
 }
+
+// round key generation (scrubbed because of indirection issues)
+void generateKeys(unsigned char *key, unsigned char ****subKeys) {
+
+}
 // convert hex to string
 unsigned char * unHexifyKey(unsigned char *key) {
 	// variables
@@ -178,7 +241,7 @@ unsigned char * unHexifyKey(unsigned char *key) {
 		// convert to ascii and store
 		rret[j] = (int)strtol(temp, NULL, 16);
 	}
-	// return
+	//
 	return rret;
 }
 // print binary
@@ -235,11 +298,73 @@ int main(int argc, char **argv) {
 			setBit(subKeys[k][2], j, getBit(subKeys[k][0], j));
 			setBit(subKeys[k][2], j + 28, getBit(subKeys[k][1], j));
 		}
-		// permutate the key into a 48 bit key
+		// calculate the permutation for the key and expand into a 48 bit key
 		doPermutation(subKeys[k][2], keyPermutations, 48);
 
-		printfB(subKeys[k][2]);
+		//printfB(subKeys[k][2]);
 	}
+
+
+	unsigned char plainText[8] = {0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF};
+	unsigned char encryptedText[8];
+
+
+
+
+    int row, col, pos = 0;
+	unsigned char leftText[6], rightText[6], funcText[6], xorText[6], subChar;
+
+	doPermutation(plainText, initialPermutation, 64);
+	// split source into two blocks
+	memcpy(leftText, &plainText[0], 4);
+	memcpy(rightText, &plainText[4], 4);
+	// iterate over the subKeys we created earlier
+	for (i = 1; i < 17; i++) {
+		// copy right portion into our function text
+		memcpy(funcText, rightText, 4);
+		// calculate the permutation for the text
+		doPermutation(funcText, expansionPermutation, 48);
+		// apply encryption
+		xorBit(funcText, subKeys[i][3], xorText, 48);
+		// copy back 6 bytes/48 bits
+		memcpy(xorText, funcText, 6);
+		// reset pos
+		pos = 0;
+		// perform the 8 sbox substitutions
+		for (j = 0; j < 8; j++) {
+			  // get rows and colums
+		      row = (getBit(funcText, (j * 6) + 0) * 2) + (getBit(funcText, (j * 6) + 5) * 1);
+		      col = (getBit(funcText, (j * 6) + 1) * 8) + (getBit(funcText, (j * 6) + 2) * 4) +
+		            (getBit(funcText, (j * 6) + 3) * 2) + (getBit(funcText, (j * 6) + 4) * 1);
+		      // get the substitution for the 6-bit output
+		      subChar = (unsigned char)sbox[j][row][col];
+		      // iterate and substitute the bits in funcText with substitutes
+		      for (k = 4; k < 8; k++) {
+		    	 // set bits for position p using k bits in subChar
+		         setBit(funcText, pos, getBit(&subChar, k));
+		         // increment pos
+		         pos++;
+		      }
+		}
+		// permute the function text with the straight pbox
+		doPermutation(funcText, pbox, 32);
+		// xor the leftText and funcText
+		xorBit(leftText, funcText, xorText, 32);
+		// copy right to left for next round
+		memcpy(leftText, rightText, 4);
+		// set the right for the next round
+		memcpy(rightText, xorText, 4);
+	}
+	// reassemble text pairs
+	memcpy(&encryptedText[0], leftText, 4);
+	memcpy(&encryptedText[1], rightText, 4);
+	// permute the final result
+	doPermutation(encryptedText, finalPermutation, 64);
+    // string out space
+    char *out = (char *)malloc(64 * sizeof(char));
+    // convert to string data
+    hexdump_to_string(encryptedText, 64, out);
+	printf("%s", out);
     // exit
     return 0;
 } 
